3620ba1148206343d278f2679a76cd1c8105a873
jhk01007 <jhk001007@naver.com>
2024-08-25 18:32:55 +0900

    fix: CheckIdDTO.java
    Json->객체로 변환안되는 문제 해결

--- src/main/java/com/example/aws/dto/CheckIdDTO.java
+++ src/main/java/com/example/aws/dto/CheckIdDTO.java
@@ -1,10 +1,18 @@
 package com.example.aws.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 public class CheckIdDTO {
     private String memberId;
+
+    @JsonCreator
+    public CheckIdDTO(@JsonProperty("memberId") String memberId) {
+        this.memberId = memberId;
+    }
 }
