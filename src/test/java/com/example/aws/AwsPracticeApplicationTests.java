d76a244db0f382fcc9ac3f12e13fcca46a6efdc0
jhk01007 <jhk001007@naver.com>
2024-10-04 17:33:10 +0900

    test: context loads 테스트 추가

---
+++ src/test/java/com/example/aws/AwsPracticeApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AwsPracticeApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
